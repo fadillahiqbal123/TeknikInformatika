3b9061968d68564cc3d3cf05b0f33c4b147aeadd
fadillahiqbal123 <fadillahharianto163@gmail.com>
2024-09-10 08:29:33 +0700

    First upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// const namaGuru = ['fadil', 'calvin', 'abi', 'rafli']
+// for (let i = 0; i < namaGuru.length; i++){
+//     console.log(namaGuru[i])
+// }
+
+let i = 10
+while(i < 10){
+    i++   
+    console.log('fadillah iqbal')
+}
+
+// let i = 10
+// do{
+//   i++
+//   console.log('fadillah iqbal')
+// }while(i < 10)
